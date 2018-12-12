f00ded577310464c0025468d420cb40b0f72884f
Ignacio <ignacio.lesci@gmail.com>
2018-12-12 16:54:09 -0300

    Init

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
+const publicPath = path.resolve(__dirname, '..', '..', 'public');
 
-app.get('/', (req, res) => {
-   res.send('Hello from Express!');
-});
+app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-   console.log('MERN Boilerplate listening on port 3000!');
+    console.log(`MERN Boilerplate listening on port 3000 and looking in folder ${publicPath}`);
 });
